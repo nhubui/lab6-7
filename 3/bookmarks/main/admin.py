2fd4d18cd093744be9fa26f361f10e1f2d542bf8
Nhu Bui <nbui@cs.ualberta.ca>
2015-02-25 19:10:42 -0700

    done lab 6

---
+++ 3/bookmarks/main/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from main.models import Link, Tag
+
+admin.site.register(Link)
+admin.site.register(Tag)
+
---
+++ 3/v1/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/cshome/nbui/Desktop/410-webdevelopment/lab6/3/v1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
